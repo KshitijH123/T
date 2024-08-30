551e961f8f4c5bffe5bc2c793b767f8e3def8c7e
KshitijH123 <kshitijhapase@gmail.com>
2024-08-30 23:02:28 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Task> _tasks = [];
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                itemCount: _tasks.length,
+                itemBuilder: (context, index) {
+                  final task = _tasks[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Card(
+                      elevation: 4.0,
+                      child: ListTile(
+                        contentPadding: const EdgeInsets.all(8.0),
+                        title: Text(task.title),
+                        subtitle: Text(task.description),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete_outlined),
+                          color: Colors.lightBlue,
+                          onPressed: () {
+                            _removeTask(index);
+                          },
+                        ),
+                        onTap: () => showEditDialog(context),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showEditDialog(context),
+        tooltip: 'Add Task',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+  void showEditDialog(BuildContext context) {
+    String title = '';
+    String description = '';
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Task'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Title',
+                ),
+                onChanged: (value) {
+                  title = value;
+                },
+              ),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                ),
+                onChanged: (value) {
+                  description = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (title.isNotEmpty && description.isNotEmpty) {
+                  _addTask(Task(title: title, description: description));
+                }
+                Navigator.of(context).pop();
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  
+  void _addTask(Task task) {
+    setState(() {
+      _tasks.add(task);
+    });
+  }
+
+  void _removeTask(int index) {
+    setState(() {
+      if (_tasks.isNotEmpty) {
+        _tasks.removeAt(index);
+      }
+    });
+  }
+
+}
+
+class Task {
+  String title;
+  String description;
+
+  Task({required this.title, required this.description});
+}
